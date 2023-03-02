ae6c16f1a9a822bc799b324078a28071400228e2
webgently <oleksandrshkabura811@gmail.com>
2023-03-03 02:28:26 +0300

    feat: site color theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,16 @@ module.exports = {
         },
         // Extend default configurations
         extend: {
-            colors: {},
+            colors: {
+                red: '#F3848A',
+                yellow: '#FAE620',
+                green: '#4CDE55',
+                blue: '#0B88D9',
+                black: '#000000',
+                'dark-blue': '#222236',
+                white: '#FFFFFF',
+                'dark-gray': '#1F1F1F',
+            },
             container: {
                 center: true,
             },
