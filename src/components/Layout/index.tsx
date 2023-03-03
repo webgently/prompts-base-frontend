9d4a4de91f9b9da590cafd30fab504328925362f
webgently <oleksandrshkabura811@gmail.com>
2023-03-03 10:48:23 +0300

    feat: import header and footer to the layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
 
 const Layout = (props: any) => {
     return (
         <div className="layout">
+            <Header />
             <main>{props.children}</main>
+            <Footer />
         </div>
     )
 }
